4b39989db4193f747416d672323797172283f352
Pier puccini <ppuccini@starkcore.com>
2019-09-18 12:41:07 -0500

    changed port to 4200

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,11 +2,46 @@
 import React from 'react'
 
 //App Imports
-import classes from './Auth.module.scss'
+// import classes from './Auth.module.scss'
 import Logo from '../../components/Logo/Logo'
-
+import Input from '../../components/UI/Input/Input'
 
 const Auth = () => {
+    const [values, setValues] = React.useState({
+        controls: {
+            email: {
+              elementType: "input",
+              elementConfig: {
+                type: "email",
+                label: "Email",
+                placeholder: 'Enter Your Email'
+              },
+              value: "",
+              validation: {
+                required: true,
+                isEmail: true
+              },
+              valid: false,
+              touched: false
+            },
+            password: {
+              elementType: "input",
+              elementConfig: {
+                type: "password",
+                label: "Password",
+                placeholder: 'Enter Your Password'
+              },
+              value: "",
+              validation: {
+                required: true,
+                minLength: 6
+              },
+              valid: false,
+              touched: false
+            }
+          },
+          isSignUp: true
+    })
     return (
         <div>
             <strong>
