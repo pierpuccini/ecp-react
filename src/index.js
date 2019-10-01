178000634a83266409695c5ca269f71985ab1763
Pier puccini <ppuccini@starkcore.com>
2019-10-01 17:20:15 -0500

    ADD: React-redux-firebase succesfully

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { reactReduxFirebase } from 'react-redux-firebase'
+import firebase from 'firebase'
+import { firebaseReducer } from 'react-redux-firebase'
 import thunk from "redux-thunk";
 
 //App imports
@@ -19,10 +22,31 @@ const composeEnhancers = process.env.NODE_ENV === "development"
 
 //All reducers must be combined here
 const rootReducer = combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  firebase: firebaseReducer
 });
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const firebaseConfig = {
+  apiKey: "AIzaSyCE4jdBGATmhgcW30ycQyJySAQrPbu88yw",
+  authDomain: "edu-coins.firebaseapp.com",
+  databaseURL: "https://edu-coins.firebaseio.com",
+  projectId: "edu-coins",
+  storageBucket: "edu-coins.appspot.com",
+  messagingSenderId: "467893607261",
+  appId: "1:467893607261:web:2be3107fa44a3c1bbb26b4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// react-redux-firebase options
+const config = {
+  userProfile: 'users', // firebase root where user profiles are stored
+  enableLogging: false, // enable/disable Firebase's database logging
+}
+// Add redux Firebase to compose
+const createStoreWithFirebase = compose(reactReduxFirebase(firebase, config))(createStore)
+
+const store = createStoreWithFirebase(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
   <Provider store={store}>
