971145b177fb580ee42ebb4e117be06dad6a607e
Pier puccini <ppuccini@starkcore.com>
2019-10-08 10:38:29 -0500

    CONVERTED: Converted Login into its own component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,170 @@
+//React Imports
+import React from "react";
+//App Imports
+import classes from "./Login.module.scss";
+import Logo from "../../components/Logo/Logo";
+import gIcon from "../../assets/svg/search.svg";
+//MaterialUI Imports
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+
+const useStyles = makeStyles(theme => ({
+  imageIcon: {
+    height: "100%"
+  },
+  iconRoot: {
+    textAlign: "center",
+    padding: "4px 12px",
+    width: "18px",
+    height: "18px",
+    "font-size": "unset"
+  },
+  container: {
+    display: "grid",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    inputAdornedEnd: "padding: unset"
+  },
+  dense: {
+    marginTop: theme.spacing(2)
+  },
+  button: {
+    margin: theme.spacing(1)
+  },
+  input: {
+    display: "none"
+  },
+  paper: {
+    padding: theme.spacing(2, 2),
+    margin: theme.spacing(2)
+  }
+}));
+
+const Login = props => {
+  const matClasses = useStyles();
+
+  return (
+    <Container maxWidth="sm">
+      <Paper className={matClasses.paper}>
+        <strong>Welcome</strong>
+        <Logo height="85px" />
+        {props.authError ? (
+          <div className={classes.loginError}>
+            {props.authError.customErrorMsg}
+          </div>
+        ) : null}
+        <div className={classes.formContainer}>
+          <form className={matClasses.container} onSubmit={props.submitHandler}>
+            <TextField
+              className={matClasses.textField}
+              label="Email"
+              placeholder="Enter Your Email"
+              type="Email"
+              value={props.authLoginForm.email.value}
+              onChange={event => props.inputChangedHandler(event, "email")}
+              margin="normal"
+              variant="outlined"
+              required
+              error={
+                !props.authLoginForm.email.valid &&
+                props.authLoginForm.email.touched
+              }
+              helperText={
+                !props.authLoginForm.email.valid &&
+                props.authLoginForm.email.touched
+                  ? "Please Enter a valid Email"
+                  : null
+              }
+            />
+            <TextField
+              className={matClasses.textField}
+              label="Password"
+              placeholder="Enter Your Password"
+              type={props.toogleViewPassword ? "text" : "password"}
+              value={props.authLoginForm.password.value}
+              onChange={event => props.inputChangedHandler(event, "password")}
+              margin="normal"
+              variant="outlined"
+              required
+              error={
+                !props.authLoginForm.password.valid &&
+                props.authLoginForm.password.touched
+              }
+              helperText={
+                !props.authLoginForm.password.valid &&
+                props.authLoginForm.password.touched
+                  ? "Please Enter a valid Password"
+                  : null
+              }
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment>
+                    <div onClick={() => props.toggleViewPasswordHandler()}>
+                      <IconButton>
+                        {props.toogleViewPassword ? (
+                          <VisibilityOffIcon />
+                        ) : (
+                          <VisibilityIcon />
+                        )}
+                      </IconButton>
+                    </div>
+                  </InputAdornment>
+                )
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              className={matClasses.button}
+              type="submit"
+              disabled={
+                !props.authLoginForm.password.valid ||
+                !props.authLoginForm.email.valid
+              }
+            >
+              Log In
+            </Button>
+          </form>
+          <div className={classes.restoreLogin}>
+            ¿Forgot your Login Details? <a href="/">Get Help Here.</a>
+          </div>
+          <div className={classes.textDivider}>
+            <span>OR</span>
+          </div>
+          <Button
+            variant="outlined"
+            className={matClasses.button}
+            onClick={event => props.submitHandler(event, "google")}
+          >
+            <Icon classes={{ root: matClasses.iconRoot }}>
+              <img
+                className={matClasses.imageIcon}
+                src={gIcon}
+                alt="google login"
+              />
+            </Icon>
+            <span className={classes.gLogin}>Sign in with Google</span>
+          </Button>
+        </div>
+      </Paper>
+      <Paper className={matClasses.paper}>
+        <div className={classes.restoreLogin}>
+          ¿Need an account? <a href="/">Sign Up.</a>
+        </div>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Login;
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,61 +6,12 @@ import * as actions from "../../store/actions/index";
 //React Router
 import { Redirect } from "react-router-dom";
 //App Imports
-import classes from "./Auth.module.scss";
-import Logo from "../../components/Logo/Logo";
-import gIcon from "../../assets/svg/search.svg";
-//MaterialUI Imports
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Paper from '@material-ui/core/Paper';
-import Icon from "@material-ui/core/Icon";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import IconButton from "@material-ui/core/IconButton";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import Login from "../../components/Login/Login";
+// import classes from "./Auth.module.scss";
 //Personal Helpers
 import { updateObject, checkValidity } from "../../shared/utility";
 
-const useStyles = makeStyles(theme => ({
-  imageIcon: {
-    height: "100%"
-  },
-  iconRoot: {
-    textAlign: "center",
-    padding: "4px 12px",
-    width: "18px",
-    height: "18px",
-    "font-size": "unset"
-  },
-  container: {
-    display: "grid",
-    flexWrap: "wrap"
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    inputAdornedEnd: "padding: unset"
-  },
-  dense: {
-    marginTop: theme.spacing(2)
-  },
-  button: {
-    margin: theme.spacing(1)
-  },
-  input: {
-    display: "none"
-  },
-  paper: {
-    padding: theme.spacing(2, 2),
-    margin: theme.spacing(2)
-  }
-}));
-
 const Auth = props => {
-  const matClasses = useStyles();
-
   const [authForm, setAuthForm] = useState({
     email: {
       value: "",
@@ -121,108 +72,16 @@ const Auth = props => {
   }
 
   return (
-    <Container maxWidth="sm">
-      <Paper className={matClasses.paper}>
-        {authRedirect}
-        <strong>Welcome</strong>
-        <Logo height="85px" />
-        {props.authError ? (
-          <div className={classes.loginError}>
-            {props.authError.customErrorMsg}
-          </div>
-        ) : null}
-        <div className={classes.formContainer}>
-          <form className={matClasses.container} onSubmit={submitHandler}>
-            <TextField
-              className={matClasses.textField}
-              label="Email"
-              placeholder="Enter Your Email"
-              type="Email"
-              value={authForm.email.value}
-              onChange={event => inputChangedHandler(event, "email")}
-              margin="normal"
-              variant="outlined"
-              required
-              error={!authForm.email.valid && authForm.email.touched}
-              helperText={
-                !authForm.email.valid && authForm.email.touched
-                  ? "Please Enter a valid Email"
-                  : null
-              }
-            />
-            <TextField
-              className={matClasses.textField}
-              label="Password"
-              placeholder="Enter Your Password"
-              type={showPassword ? "text" : "password"}
-              value={authForm.password.value}
-              onChange={event => inputChangedHandler(event, "password")}
-              margin="normal"
-              variant="outlined"
-              required
-              error={!authForm.password.valid && authForm.password.touched}
-              helperText={
-                !authForm.password.valid && authForm.password.touched
-                  ? "Please Enter a valid Password"
-                  : null
-              }
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment>
-                    <div onClick={() => toggleViewPasswordHandler()}>
-                      <IconButton>
-                        {showPassword ? (
-                          <VisibilityOffIcon />
-                        ) : (
-                          <VisibilityIcon />
-                        )}
-                      </IconButton>
-                    </div>
-                  </InputAdornment>
-                )
-              }}
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              className={matClasses.button}
-              type="submit"
-              disabled={
-                (!authForm.password.valid && authForm.password.touched) ||
-                (!authForm.email.valid && authForm.email.touched)
-              }
-            >
-              Log In
-            </Button>
-          </form>
-          <div className={classes.restoreLogin}>
-            ¿Forgot your Login Details? <a href="/">Get Help Here.</a>
-          </div>
-          <div className={classes.textDivider}>
-            <span>OR</span>
-          </div>
-          <Button
-            variant="outlined"
-            className={matClasses.button}
-            onClick={event => submitHandler(event, "google")}
-          >
-            <Icon classes={{ root: matClasses.iconRoot }}>
-              <img
-                className={matClasses.imageIcon}
-                src={gIcon}
-                alt="google login"
-              />
-            </Icon>
-            <span className={classes.gLogin}>Sign in with Google</span>
-          </Button>
-        </div>
-      </Paper>
-      <Paper className={matClasses.paper}>
-        <div className={classes.restoreLogin}>
-          ¿Need an account? <a href="/">Sign Up.</a>
-        </div>
-      </Paper>
-    </Container>
+    <div>
+      {authRedirect}
+      <Login
+        submitHandler={submitHandler}
+        authLoginForm={authForm}
+        inputChangedHandler={inputChangedHandler}
+        toogleViewPassword={showPassword}
+        toggleViewPasswordHandler={toggleViewPasswordHandler}
+      />
+    </div>
   );
 };
 
