d5e081843bd74c3753fc3527f3ee8eb1f5d4170a
ppuccini <ppuccini@starkcore.com>
2019-10-18 15:53:23 -0500

    Succesfully implemented Top Bar, Missing bottom bar for mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 //React Imports
 import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+//Redux Imports
 import { connect } from "react-redux";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
+//Material UI Imports
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 //component Imports
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 import "./App.css";
 import loader from "./assets/loaders/educoin(B).gif";
 
@@ -14,9 +23,24 @@ const asyncDashboard = asyncComponent(() => {
   return import("./containers/Dashboard/Dashboard");
 });
 
-function App(props) {
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  });
 
-  const [domReady, setDomReady] = useState(false)
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired
+};
+
+function App(props) {
+  const [domReady, setDomReady] = useState(false);
 
   useEffect(() => {
     let showCoinLoader = setTimeout(() => {
@@ -32,16 +56,19 @@ function App(props) {
       <img src={loader} alt="loading..." />
     </div>
   );
-  
+
   /* Routes for non-authenticated users */
-  let routes = (
-    <Switch>
-      <Route path="/login" component={asyncAuth} />
-      <Route path="/sign-up" component={asyncAuth} />
-      <Route path="/forgot-login" component={asyncAuth} />
-      <Redirect to="/login" />
-    </Switch>
+  let app = (
+    <div className="App">
+      <Switch>
+        <Route path="/login" component={asyncAuth} />
+        <Route path="/sign-up" component={asyncAuth} />
+        <Route path="/forgot-login" component={asyncAuth} />
+        <Redirect to="/login" />
+      </Switch>
+    </div>
   );
+  let routes = null;
   /* Routes for authenticated users */
   if (props.isAuthenticated) {
     routes = (
@@ -56,13 +83,44 @@ function App(props) {
         <Redirect to={`${process.env.PUBLIC_URL}/home`} />
       </Switch>
     );
+
+    app = (
+      <React.Fragment>
+        <ElevationScroll {...props}>
+          <AppBar>
+            <Toolbar>
+              <Typography variant="h6">Scroll to Elevate App Bar</Typography>
+            </Toolbar>
+          </AppBar>
+        </ElevationScroll>
+        <Toolbar />
+        <Container>
+        {routes}
+          <Box my={2}>
+            {[...new Array(12)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+              )
+              .join("\n")}
+          </Box>
+        </Container>
+      </React.Fragment>
+    );
   }
-  return <div className="App">{(domReady)?routes:loadingDom}</div>;
+
+  return <React.Fragment>{domReady ? app : loadingDom}</React.Fragment>;
 }
 
 const mapStateToProps = state => {
   return {
-    isAuthenticated: state.firebase.auth.uid && !state.auth.newUser && !state.auth.isGoogleSignUp && state.auth.isPhoneLinkSucces
+    isAuthenticated:
+      state.firebase.auth.uid &&
+      !state.auth.newUser &&
+      !state.auth.isGoogleSignUp &&
+      state.auth.isPhoneLinkSucces
   };
 };
 
