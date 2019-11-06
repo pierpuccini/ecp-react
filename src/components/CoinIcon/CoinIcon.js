157dff3bb6b0a0c11df439112b07430bb1a1847a
Pier puccini <ppuccini@starkcore.com>
2019-11-06 12:12:07 -0500

    Created coin icon component

---
+++ src/components/CoinIcon/CoinIcon.js
@@ -0,0 +1,29 @@
+//React Impors
+import React from "react";
+//Material UI
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+//App icons
+import coinIcon from "../../assets/images/coin-logo.png";
+
+const useStyles = makeStyles(theme => ({
+  imageIcon: {
+    height: "100%"
+  },
+  iconRootCoin: {
+    textAlign: "center",
+    "font-size": "unset"
+  }
+}));
+
+const CoinIcon = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Icon classes={{ root: classes.iconRootCoin }} style={{width: props.width, height: props.height}}>
+      <img className={classes.imageIcon} src={coinIcon} alt="coin icon" />
+    </Icon>
+  );
+};
+
+export default CoinIcon;
