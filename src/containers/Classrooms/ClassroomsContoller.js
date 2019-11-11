5660d50f992aef914b0eaa672e4af46d8505316c
Pier puccini <ppuccini@starkcore.com>
2019-11-11 17:02:58 -0500

    Adding routes

--- src/containers/Classrooms/ClassroomsContoller.js
+++ src/containers/Classrooms/ClassroomsContoller.js
@@ -1,15 +1,17 @@
 /* React imports */
 import React, { useState } from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 /* Material Imports */
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 //Icons
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import ListOutlinedIcon from "@material-ui/icons/ListOutlined";
 /* App imports */
 import asyncComponent from "../../hoc/asyncComponent/asyncComponent";
 
@@ -19,8 +21,10 @@ const createClassroom = asyncComponent(() => {
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: "flex",
-    flexDirection: "column"
+    padding: "unset !important",
+    [theme.breakpoints.up("md")]: {
+      minWidth: "685px !important"
+    }
   },
   paper: {
     padding: theme.spacing(2, 2),
@@ -30,18 +34,38 @@ const useStyles = makeStyles(theme => ({
     }
   },
   button: {
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
+    [theme.breakpoints.down("xs")]: {
+      minWidth: "200px"
+    },
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  actionButtonsContainer: {
+    display: "flex",
+    justifyContent: "space-around",
+    flexWrap: "wrap"
+  },
+  classroomListHeader: {
+    display: "flex"
   }
 }));
 
 const ClassroomController = props => {
   const classes = useStyles();
   const [navRoute, setNavRoute] = useState("/classrooms");
+
+  const handleNavChange = (event, newValue) => {
+    setNavRoute(newValue);
+  };
   /* Define new routes in routes array with their url and corresponding component */
-  let routes;
+  let routes, redirect;
   const routesArray = [
     { url: "create", comp: createClassroom, restriction: "student" }
   ];
+
+  /* Conditional routes section */
+  redirect = <Redirect to={`/${navRoute}`} />;
 
   //Available routes or Guarded routes
   routes = (
@@ -59,6 +83,7 @@ const ClassroomController = props => {
           return null;
         }
       })}
+      {redirect}
     </Switch>
   );
 
@@ -68,12 +93,13 @@ const ClassroomController = props => {
         <Container maxWidth="sm" className={classes.container}>
           <Paper className={classes.paper}>
             <Typography>Classroom Manager</Typography>
-            <div>
+            <div className={classes.actionButtonsContainer}>
               <Button
                 variant="contained"
                 color="primary"
                 className={classes.button}
-                startIcon={<AddCircleOutlineOutlinedIcon />}
+                endIcon={<AddCircleOutlineOutlinedIcon />}
+                onClick={(event)=>{handleNavChange(event,'create')}}
               >
                 Create Classroom
               </Button>
@@ -81,12 +107,20 @@ const ClassroomController = props => {
                 variant="contained"
                 color="primary"
                 className={classes.button}
-                startIcon={<EditOutlinedIcon />}
+                endIcon={<EditOutlinedIcon />}
+                onClick={()=>{handleNavChange('edit')}}
               >
                 Edit Classroom
               </Button>
             </div>
-            <p>list of classrooms</p>
+            <div>
+              <div className={classes.classroomListHeader}>
+                <Icon style={{marginRight: "5px"}}>
+                  <ListOutlinedIcon />
+                </Icon>
+                <Typography>Classroom List</Typography>
+              </div>
+            </div>
           </Paper>
         </Container>
       ) : (
