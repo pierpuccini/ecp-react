f49b3b18908eaf26ff45c29b8f6808c11b49a8d9
pierpuccini <pierpuccin@gmail.com>
2019-11-24 14:01:12 -0500

    Connected to development classroom api

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+// Master url
+// const instance = axios.create({
+//     baseURL: 'https://api-educoins-git-master.educoins.now.sh/'
+// });
+
+//TODO: classroom api, remove once in master
 const instance = axios.create({
-    baseURL: 'https://api-educoins-git-master.educoins.now.sh/'
+    baseURL: 'https://api-educoins-git-classroomcrud.educoins.now.sh/'
 });
 
 export default instance;
