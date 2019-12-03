1d159ec7c5f8f341c3fe2fcb77051b7ff65a9907
Pier puccini <ppuccini@starkcore.com>
2019-12-03 13:06:13 -0500

    Added real time filter to user manager

--- src/components/Users/UserManager/UserManager.js
+++ src/components/Users/UserManager/UserManager.js
@@ -127,7 +127,7 @@ const UserManager = props => {
     }
     /* MISSING DEPENDENCIES: teachers, students */
     // eslint-disable-next-line
-  }, [checkboxState]);
+  }, [checkboxState, teachers, students]);
 
   /* USER CARD IS A SMART COMPONENT IN CASE ERROR ARRISES FROM THERE */
   return (
