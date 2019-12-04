c27fc1815e5ca9a155690037cab28b4446fb290e
Pier puccini <ppuccini@starkcore.com>
2019-12-04 11:16:50 -0500

    Added development and localhost urls

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
 // Master url
+// const instance = axios.create({
+//     baseURL: 'https://api-educoins.educoins.now.sh/'
+// });
+//Development
+// const instance = axios.create({
+//     baseURL: 'https://api-educoins-6zcuwfgv4.now.sh/'
+// });
+//Local host
 const instance = axios.create({
-    baseURL: 'https://api-educoins.educoins.now.sh/'
+    baseURL: 'http://127.0.0.1:3333/'
 });
+
 
 export default instance;
