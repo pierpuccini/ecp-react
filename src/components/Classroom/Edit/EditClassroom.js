c233efd235b1ffd36c79d19eccb5cbd0fdb681c2
Pier puccini <ppuccini@starkcore.com>
2019-12-10 16:39:48 -0500

    Added classroom tabs

--- src/components/Classroom/Edit/EditClassroom.js
+++ src/components/Classroom/Edit/EditClassroom.js
@@ -1,12 +1,12 @@
 /* React Imports */
 import React, { useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 /* Material Imports */
 import { makeStyles } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import Box from '@material-ui/core/Box';
+import Box from "@material-ui/core/Box";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 /* App Imports */
@@ -17,6 +17,7 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(1)
   },
   paper: {
+    overflow: "auto",
     padding: theme.spacing(2, 2),
     margin: theme.spacing(2),
     border: "unset",
@@ -32,12 +33,8 @@ function TabPanel(props) {
   const { children, value, index } = props;
 
   return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
+    <Typography component="div" role="tabpanel" hidden={value !== index}>
+      {value === index && <Box p={2}>{children}</Box>}
     </Typography>
   );
 }
@@ -88,13 +85,10 @@ const EditClassroom = props => {
       className={classes.paper}
       style={prefersDarkMode ? { border: "unset" } : null}
     >
-      <Tabs
-        value={tabValue}
-        onChange={handleTabChange}
-      >
-        <Tab label="Classroom Info" />
-        <Tab label="Classroom Roster" />
-        <Tab label="Classroom Wallet" disabled />
+      <Tabs value={tabValue} onChange={handleTabChange} variant="fullWidth">
+        <Tab label="Info" />
+        <Tab label="Roster" />
+        <Tab label="Wallet" disabled />
       </Tabs>
       <TabPanel value={tabValue} index={0}>
         <EditClassroomFields
