54fff09136203b3c07ef53fa1d4dab90cdbf71b0
pierpuccini <pierpuccini@gmail.com>
2019-12-23 01:34:09 -0500

    Correcting axios deploy

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 /* Master url */
-// const instance = axios.create({
-//     baseURL: 'https://educoins.herokuapp.com/'
-// });
+const instance = axios.create({
+    baseURL: 'https://educoins.herokuapp.com/'
+});
 
 /* Local host */
-const instance = axios.create({
-    baseURL: 'http://127.0.0.1:3333/'
-});
+// const instance = axios.create({
+//     baseURL: 'http://127.0.0.1:3333/'
+// });
 
 /* Local network */
 // const instance = axios.create({
